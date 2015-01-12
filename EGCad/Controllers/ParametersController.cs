092914f9c6de317c502a7a4cf15f12012a635b38
Denis Chudinov <denischudinovg@gmail.com>
2015-01-13 00:33:32 +0400

    parameter editor

--- EGCad/Controllers/ParametersController.cs
+++ EGCad/Controllers/ParametersController.cs
@@ -17,23 +17,30 @@ namespace EGCad.Controllers
             return Data(0, new { items = Parameters.ToArray() });
         }
 
-        public JsonResult Save(string name, string unit)
+        public JsonResult Save(string name, string unit, int? id)
         {
-            var param = Parameters;
-           
-            param.Add(new Parameter(Parameters.Count, name, unit));
-            Parameters = param;
-
+            if (id.HasValue)
+            {
+                var param = Parameters[id.Value];
+                param.Name = name;
+                param.Unit = unit;
+            }
+            else
+            {
+                var param = Parameters;
+                param.Add(new Parameter(Parameters.Count, name, unit));
+                Parameters = param;
+            }
             return GetState();
         }
 
         public JsonResult Remove(int id)
         {
             var param = Parameters;
-            
+
             param.Remove(param.FirstOrDefault(x => x.Id == id));
             param.ForEach(x => x.Id = param.IndexOf(x));
-            
+
             Parameters = param;
 
             return GetState();
