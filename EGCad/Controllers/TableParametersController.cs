09759a7daff0b1bd168238244cbee496d1ecf154
Denis Chudinov <denischudinovg@gmail.com>
2015-01-25 18:18:49 +0400

    end develop 3 step

--- EGCad/Controllers/TableParametersController.cs
+++ EGCad/Controllers/TableParametersController.cs
@@ -2,79 +2,96 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
+using EGCad.Common.Extensions;
 using EGCad.Models.InputData;
 
 namespace EGCad.Controllers
 {
-    public class TableParametersController : InputBaseController
-    {
-        // GET: TableParameters
-        public ActionResult Index()
-        {
-            if (!Points.Any())
-            {
-                Points = new List<ParameterTableEntry>(new[] {new ParameterTableEntry(0, 0, 0, Parameters)});
-            }
-            else
-            {
-                foreach (var parameter in Parameters)
-                {
-                    if (Points[0].Parameters.All(x => x.Id != parameter.Id))
-                    {
-                        var points = Points;
-                        points.ForEach(p=>p.Parameters.Add(parameter));
-                        Points=points;
-                    }
-                }
-            }
-            return View(Parameters);
-        }
+	public class TableParametersController : InputBaseController
+	{
+		// GET: TableParameters
+		public ActionResult Index()
+		{
+			if (!Points.Any())
+			{
+				Points = new List<ParameterTableEntry>(new[] { new ParameterTableEntry(0, 0, 0, Parameters) });
+			}
+			else
+			{
+				foreach (var point in Points)
+				{
+					var pointParameters = point.Parameters;
+					var currentParametes = Parameters;
+					var actualParameters = new List<Parameter>();
 
-        public JsonResult GetState()
-        {
-            return Data(0, new { items = Points.ToArray() });
-        }
+					foreach (var parameter in currentParametes)
+					{
+						var pointParam = pointParameters.FirstOrDefault(x => x.Id == parameter.Id);
+						actualParameters.Add(pointParam ?? parameter);
+					}
 
-        public JsonResult CreatePoint()
-        {
-            var points = Points;
-            points.Add(new ParameterTableEntry(Points.Last().Id+1, 0, 0, Parameters));
-            Points = points;
-            return GetState();
-        }
+					point.Parameters = actualParameters;
+				}
+			}
+			return View(Parameters);
+		}
 
-        public JsonResult Save(int x, int y, int id, double[] values)
-        {
-            var points = Points;
+		public JsonResult GetState()
+		{
+			return Data(0, new { items = Points.ToArray() });
+		}
 
-            if (Parameters.Count != values.Count()) throw new ArgumentException("Number of values not equal parameters count!");
+		public JsonResult CreatePoint()
+		{
+			var points = Points;
+			points.Add(new ParameterTableEntry(Points.Last().Id + 1, 0, 0, Parameters));
+			Points = points;
+			return GetState();
+		}
 
-            var parameters = values.Select((value, i) => new Parameter(Parameters[i].Id, Parameters[i].Name, Parameters[i].Unit, value)).ToList();
+		public JsonResult Replace(bool direction, int id)
+		{
+			var points = Points;
+			points.Swap(id, direction ? id - 1 : id + 1);
+			points.ForEach(p => p.Id = points.IndexOf(p));
+			Points = points;
+			return GetState();
+		}
 
-            var point = points.First(p => p.Id == id);
-            point.Parameters = parameters;
-            point.X = x;
-            point.Y = y;
-            Points = points;
+		public JsonResult Save(int x, int y, int id, double[] values)
+		{
+			var points = Points;
+		    if (Parameters.Any())
+		    {
+		        if (Parameters.Count != values.Count())
+		            throw new ArgumentException("Number of values not equal parameters count!");
+		    }
+		    var parameters = values.Select((value, i) => new Parameter(Parameters[i].Id, Parameters[i].Name, Parameters[i].Unit, value)).ToList();
 
-            return GetState();
-        }
+			var point = points.First(p => p.Id == id);
+			point.Parameters = parameters;
+			point.X = x;
+			point.Y = y;
+			Points = points;
 
-        public JsonResult Remove(int id)
-        {
-            var points = Points;
+			return GetState();
+		}
 
-            points.Remove(points.FirstOrDefault(x => x.Id == id));
-            points.ForEach(x => x.Id = points.IndexOf(x));
+		public JsonResult Remove(int id)
+		{
+			var points = Points;
 
-            Points = points;
+			points.Remove(points.FirstOrDefault(x => x.Id == id));
+			points.ForEach(x => x.Id = points.IndexOf(x));
 
-            return GetState();
-        }
+			Points = points;
 
-        private List<ParameterTableEntry> GetParameterTableEntries(List<Parameter> parameters)
-        {
-            return new List<ParameterTableEntry>(new[] { new ParameterTableEntry(0, 0, 0, parameters) });
-        }
-    }
+			return GetState();
+		}
+
+		private List<ParameterTableEntry> GetParameterTableEntries(List<Parameter> parameters)
+		{
+			return new List<ParameterTableEntry>(new[] { new ParameterTableEntry(0, 0, 0, parameters) });
+		}
+	}
 }
--- egcad-master/EGCad/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EGCad.MvcApplication" Language="C#" %>
