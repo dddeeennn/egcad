3a620d60dd4d266601500e24bea32cbcad261bad
Denis Chudinov <denischudinovg@gmail.com>
2015-02-23 13:34:56 +0400

    map scale bugfix

--- EGCad.Common/Model/Data/Map.cs
+++ EGCad.Common/Model/Data/Map.cs
@@ -4,124 +4,124 @@ using System.Linq;
 
 namespace EGCad.Common.Model.Data
 {
-	/// <summary>
-	/// input map 
-	/// </summary>
-	public class Map
-	{
-		private const double Tolerance = 0.000000000000001;
+    /// <summary>
+    /// input map 
+    /// </summary>
+    public class Map
+    {
+        private const double Tolerance = 0.000000000000001;
 
-		public double K
-		{
-			get
-			{
-				if (Start.IsEmpty || End.IsEmpty) return 0;
-				return (double)(End.Y - Start.Y) / (End.X - Start.X);
-			}
-		}
+        public double K
+        {
+            get
+            {
+                if (Start.IsEmpty || End.IsEmpty) return 0;
+                return (double)(End.Y - Start.Y) / (End.X - Start.X);
+            }
+        }
 
-		public double B
-		{
-			get
-			{
-				if (Start.IsEmpty || End.IsEmpty) return 0;
-				return (double)(End.X * Start.Y - End.Y * Start.X) / (End.X - Start.X);
-			}
-		}
+        public double B
+        {
+            get
+            {
+                if (Start.IsEmpty || End.IsEmpty) return 0;
+                return (double)(End.X * Start.Y - End.Y * Start.X) / (End.X - Start.X);
+            }
+        }
 
-		//for real-world map
-		public double BT
-		{
-			get { return B * ScaleKoef; }
-		}
+        //for real-world map
+        public double BT
+        {
+            get { return B * ScaleKoef; }
+        }
 
-		public Map(Image image, string src, Point start, Point end)
-			: this()
-		{
-			ImgSrc = src;
-			Image = image;
-			Start = start;
-			End = end;
-		}
+        public Map(Image image, string src, Point start, Point end)
+            : this()
+        {
+            ImgSrc = src;
+            Image = image;
+            Start = start;
+            End = end;
+        }
 
-		public Map()
-		{
-			StartT = new Point(0, 0);
-		}
+        public Map()
+        {
+            StartT = new Point(0, 0);
+        }
 
-		public string ImgSrc { get; set; }
+        public string ImgSrc { get; set; }
 
-		public Image Image { get; set; }
+        public Image Image { get; set; }
 
-		public int Width
-		{
-			get { return Image != null ? Image.Width : 0; }
-		}
+        public int Width
+        {
+            get { return Image != null ? Image.Width : 0; }
+        }
 
-		public int Height
-		{
-			get { return Image != null ? Image.Height : 0; }
-		}
+        public int Height
+        {
+            get { return Image != null ? Image.Height : 0; }
+        }
 
-		//canvas coord
-		public Point Start { get; set; }
-		public Point End { get; set; }
+        //canvas coord
+        public Point Start { get; set; }
+        public Point End { get; set; }
 
-		//topology from real map
-		public Point StartT { get; set; }
-		public Point EndT { get; set; }
+        //topology from real map
+        public Point StartT { get; set; }
+        public Point EndT { get; set; }
 
-		public double WidthT
-		{
-			get
-			{
-				if (Start.IsEmpty || End.IsEmpty || EndT.IsEmpty) return 0;
-				return Width * Math.Abs(StartT.X - EndT.X) / (double)Math.Abs(Start.X - End.X);
-			}
-		}
+        public double WidthT
+        {
+            get
+            {
+                if (Start.IsEmpty || End.IsEmpty || EndT.IsEmpty) return 0;
+                return Width * Math.Abs(StartT.X - EndT.X) / (double)Math.Abs(Start.X - End.X);
+            }
+        }
 
-		public double HeightT
-		{
-			get
-			{
-				if (Math.Abs(AspectRatio) < Tolerance || Math.Abs(WidthT) < Tolerance) return 0;
-				return WidthT / AspectRatio;
-			}
-		}
+        public double HeightT
+        {
+            get
+            {
+                if (Math.Abs(AspectRatio) < Tolerance || Math.Abs(WidthT) < Tolerance) return 0;
+                return WidthT / AspectRatio;
+            }
+        }
 
-		public double AspectRatio
-		{
-			get
-			{
-				if (Width == 0 || Height == 0) return 0;
-				return (double)Width / Height;
-			}
-		}
+        public double AspectRatio
+        {
+            get
+            {
+                if (Width == 0 || Height == 0) return 0;
+                return (double)Width / Height;
+            }
+        }
 
-		/// <summary>
-		/// Get scale koef m/px
-		/// </summary>
-		/// <value>
-		/// The scale koef.
-		/// </value>
-		public double ScaleKoef
-		{
-			get { return GetScaleKoef(); }
-		}
+        /// <summary>
+        /// Get scale koef m/px
+        /// </summary>
+        /// <value>
+        /// The scale koef.
+        /// </value>
+        public double ScaleKoef
+        {
+            get { return GetScaleKoef(); }
+        }
 
-		public bool IsValid
-		{
-			get { return Image != null && !Start.IsEmpty && !End.IsEmpty && !EndT.IsEmpty; }
-		}
+        public bool IsValid
+        {
+            get { return Image != null && !Start.IsEmpty && !End.IsEmpty && !EndT.IsEmpty; }
+        }
 
-		public double GetScaleKoef()
-		{
-			if (Start.IsEmpty || End.IsEmpty || EndT.IsEmpty) return 0;
+        public double GetScaleKoef()
+        {
+            if (Start.IsEmpty || End.IsEmpty || EndT.IsEmpty) return 0;
 
-			double m = StartT.X - EndT.X;
-			double px = Start.X - End.X;
+            double m = StartT.X - EndT.X;
+            double px = Math.Sqrt(Math.Pow((Start.X - End.X), 2) + Math.Pow((Start.Y - End.Y), 2));
 
-			return Math.Abs(m / px);
-		}
-	}
+            return Math.Abs(m / px);
+        }
+    }
 }
--- EGCad/Global.asax.cs
+++ EGCad/Global.asax.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -9,7 +11,7 @@ namespace EGCad
 {
 	public class MvcApplication : HttpApplication
 	{
-		private static NLog.Logger logger = LogManager.GetCurrentClassLogger();
+		private static Logger logger = LogManager.GetCurrentClassLogger();
 
 		protected void Application_Start()
 		{
@@ -25,8 +27,41 @@ namespace EGCad
 				bundle.Transforms.Clear();
 			}
 #endif
+
 		}
 
+        public void Application_End()
+        {
+
+            var runtime = (HttpRuntime)typeof(HttpRuntime).InvokeMember("_theRuntime",
+                                                                        BindingFlags.NonPublic
+                                                                        | BindingFlags.Static
+                                                                        | BindingFlags.GetField,
+                                                                        null,
+                                                                        null,
+                                                                        null);
+
+            if (runtime == null)
+                return;
+
+            var shutDownMessage = (string)runtime.GetType().InvokeMember("_shutDownMessage",
+                                                                             BindingFlags.NonPublic
+                                                                             | BindingFlags.Instance
+                                                                             | BindingFlags.GetField,
+                                                                             null,
+                                                                             runtime,
+                                                                             null);
+
+            var shutDownStack = (string)runtime.GetType().InvokeMember("_shutDownStack",
+                                                                           BindingFlags.NonPublic
+                                                                           | BindingFlags.Instance
+                                                                           | BindingFlags.GetField,
+                                                                           null,
+                                                                           runtime,
+                                                                           null);
+
+            logger.Error("\r\n\r\n_shutDownMessage={0}\r\n\r\n_shutDownStack={1}", shutDownMessage, shutDownStack);
+        }
 
 		protected void Session_OnEnd(object sender, EventArgs e)
 		{
