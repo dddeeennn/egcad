107e8f4808dc6a7772359027526c895431ec976a
Denis Chudinov <denischudinovg@gmail.com>
2015-03-13 00:19:08 +0400

    fix for release, cluster tree fix and color on result map

--- EGCad/Models/DrawPoint.cs
+++ EGCad/Models/DrawPoint.cs
@@ -1,21 +1,24 @@
 ﻿namespace EGCad.Models
 {
-	public class DrawPoint
-	{
-		public int X { get; private set; }
+    public class DrawPoint
+    {
+        public int X { get; private set; }
 
-		public int Y { get; private set; }
+        public int Y { get; private set; }
 
-		public string Title { get; private set; }
+        public string Title { get; private set; }
 
-		public bool IsNew { get; private set; }
+        public bool IsNew { get; private set; }
 
-		public DrawPoint(int x, int y, string title, bool isNew)
-		{
-			X = x;
-			Y = y;
-			IsNew = isNew;
-			Title = title;
-		}
-	}
+        public byte ClusterIndex { get; private set; }
+
+        public DrawPoint(int x, int y, string title, byte clusterIndex, bool isNew)
+        {
+            X = x;
+            Y = y;
+            IsNew = isNew;
+            Title = title;
+            ClusterIndex = IsNew ? (byte)255:clusterIndex;
+        }
+    }
 }
